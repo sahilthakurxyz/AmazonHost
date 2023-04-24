6fee3c7a857e00920508b0d64f3d8afa22d39b24
sahil thakur <sahilthakurxyz@gmail.com>
2023-04-25 00:22:01 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.c50d12e581d954b6e1cc4e1a5b081d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8657cc6a77fd7b6d63d",
+    "url": "/AmazonHost/static/css/main.bc96b901.chunk.css"
+  },
+  {
+    "revision": "e8657cc6a77fd7b6d63d",
+    "url": "/AmazonHost/static/js/main.caf41b48.chunk.js"
+  },
+  {
+    "revision": "1f1382ffa81d2eb34fbe",
+    "url": "/AmazonHost/static/js/runtime~main.4a28d3f2.js"
+  },
+  {
+    "revision": "c8f6e371f8ec7cb8ff80",
+    "url": "/AmazonHost/static/js/2.bbefc2eb.chunk.js"
+  },
+  {
+    "revision": "d1d6b6c654f7ea6a624c",
+    "url": "/AmazonHost/static/js/3.d792b4f0.chunk.js"
+  },
+  {
+    "revision": "6ac07d6ec97e86675f2278c4642efd5b",
+    "url": "/AmazonHost/index.html"
+  }
+];
